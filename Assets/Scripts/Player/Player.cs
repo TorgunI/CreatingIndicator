fcf25b1556187593dabaf498960e74e23ebf63b4
TorgunI <torgunivan@gmail.com>
2022-02-10 19:41:24 +0300

    FinalFix

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,16 +6,20 @@ using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
-    public UnityAction HealthChanged;
 
     public float Health { get; private set; } = 50;
     public float MinHealth { get; private set; } = 0;
     public float MaxHealth { get; private set; } = 100;
 
+    public event UnityAction HealthChanged;
+
     public void Heal(float healValue)
     {
         if (IsUpperLimitHealth() == false)
         {
+            if (healValue >= MaxHealth)
+                healValue = MaxHealth;
+
             Health += healValue;
             HealthChanged?.Invoke();
         }
@@ -23,7 +27,7 @@ public class Player : MonoBehaviour
 
     public void Damage(float damageValue)
     {
-        if (IsLowerLimitHealth() == false)
+        if (IsLowerLimitHealth() == false && damageValue <= Health)
         {
             Health -= damageValue;
             HealthChanged?.Invoke();
