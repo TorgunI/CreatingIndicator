f09007a05ad199724ce2e6c8afdc0883f9211d6a
TorgunI <torgunivan@gmail.com>
2022-02-10 13:00:29 +0300

    fix

---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Player _player;
+
+    private Slider _slider;
+
+    private const float deltaValue = 25;
+
+    private void Awake()
+    {
+        _slider = GetComponent<Slider>();
+        _slider.minValue = 0;
+        _slider.maxValue = 100;
+        _slider.value = _player.Health;
+    }
+
+    private void Update()
+    {
+        if(_slider.value != _player.Health)
+            _slider.value = Mathf.MoveTowards(_slider.value, _player.Health, deltaValue * Time.deltaTime);
+    }
+}
